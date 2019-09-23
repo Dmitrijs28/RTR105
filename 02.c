957099d8612f91800c264782c2ed6a46cd95cc05
user <blackbizon45@gmail.com>
2019-09-23 18:09:32 +0300

    20190923

---
+++ 02.c
@@ -0,0 +1,13 @@
+
+#include<stdio.h>
+
+void  main ()
+{
+printf("TekstsNr1\n\r");
+printf("TekstsNr2\n\t");
+printf("TekstsNr3\n\a");
+printf("TekstsNr4\n\'");
+printf("TekstsNr4\n\"");
+printf("TekstsNr4\n\?");
+printf("TekstsNr4\n\\");
+}
